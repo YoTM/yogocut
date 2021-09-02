f7c7f5763224834db8e8f5d1a4433211b66bc9c2
yolkin <etm@a-real.ru>
2021-09-02 18:28:30 +0300

    feat: add ui elements and window size

--- main.go
+++ main.go
@@ -1,24 +1,69 @@
-//go:generate fyne bundle -o logo.png
+//go:generate fyne bundle -o data.go Icon.png
 
 package main
 
 import (
+	"fmt"
+	"log"
+	"os/user"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Hello")
+	myApp := app.New()
+	myWindow := myApp.NewWindow("Yogocut v.0.2")
+	myWindow.SetIcon(resourceLogoPng)
+	myWindow.CenterOnScreen()
+	myWindow.Resize(fyne.NewSize(600, 300))
 
-	hello := widget.NewLabel("Hello Fyne!")
-	w.SetContent(container.NewVBox(
-		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-	))
+	// Получаем данные текущего пользователя.
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	w.ShowAndRun()
+	// Youtube Link input
+	linkInput := widget.NewEntry()
+	linkInput.SetPlaceHolder("Enter Youtube link here...")
+
+	// Start time input
+	startInput := widget.NewEntry()
+	startInput.SetPlaceHolder("Enter start time of video...")
+
+	// Finish time input
+	finishInput := widget.NewEntry()
+	finishInput.SetPlaceHolder("Enter finish time of video...")
+
+	// Result output path
+	resOutput := widget.NewEntry()
+	resOutput.SetText("Donwload to: " + usr.HomeDir)
+
+	downloadBtn := widget.NewButton(
+		"Download", 
+		func(){downloadVideo(resOutput, "http://youtu.be/video")},
+	)
+
+	content := container.NewVBox(
+		linkInput,
+		startInput,
+		finishInput, 
+		resOutput, 
+		downloadBtn,
+	)
+
+	myWindow.SetContent(content)
+	myWindow.ShowAndRun()
+	tidyUp()
+}
+
+func tidyUp() {
+	fmt.Println("Exited")
+}
+
+func downloadVideo(obj *widget.Entry, link string) {
+	obj.SetText(link + " completed!")
 }
